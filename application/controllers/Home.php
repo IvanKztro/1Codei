68419ae72afd217ab5f158a5e79d8fb1b2f4b188
Ivan Castro <ivanc654gmail.com>
2019-09-21 17:26:17 -0500

    Agrego cards y modelo de menu vertical izquierdo

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -6,6 +6,7 @@ class Home extends CI_Controller
 
 	function index()
 	{
+		$this->load->helper('html');
 		$datos = array("title" => "Principal", "h2" => "Inicio de sesion", "p" => "hola mundo");
 		$this->load->view("home.html",$datos);
 	}
